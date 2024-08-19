d86d8792f5d019da1872c2c70e9f5081b02bebe9
Anacasconi1 <sabriicasconii@gmail.com>
2024-08-19 19:46:15 -0300

    UPDATE laCajadePandora function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if(numero % 2 == 0) {
+        let almacenador = []
+        while (numero!== 0){
+            let residuo = parseInt(numero%2)
+            almacenador.push(residuo);
+        }
+        return almacenador
     }
+    return String(numero)
+    }
+
+laCajaDePandora(8)

570635f3ab7263b48912a65b7f92a00ca49fa942
MPCarranza <mariapilarcarranza@gmail.com>
2024-08-19 20:42:21 -0300

    add nombre Pili

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function nombre() {
+    return {
+        nombre: "Maria Pilar Carranza",
+        edad: "30",
+        nacionalidad: "Argentina"
     }
+}
+
+console.log(nombre())

cb36c042ab132f575240b3d4929b3cf18d7db674
SamuelCastrillon <samcastj@gmail.com>
2024-08-19 17:37:36 -0500

    update Function Index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
+function laCajaDePandora(numero) {
+  if (numero % 2 == 0) {
+    console.log(numero.toString(2));
+    return;
+  } else {
+    console.log(numero.toString(16));
+
+    return;
+  }
+}
+laCajaDePandora(45);

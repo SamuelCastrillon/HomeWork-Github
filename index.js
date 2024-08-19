ea87a6b824274dfa88590ac92550ec7613838fff
SamuelCastrillon <165056887+SamuelCastrillon@users.noreply.github.com>
2024-08-19 18:27:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-
-function laCajaDePandora(numero) {
-  if (numero % 2 == 0) {
-    console.log(numero.toString(2));
-    return;
-  } else {
-    console.log(numero.toString(16));
-
-    return;
-  }
-}
-laCajaDePandora(45);
-
 function laCajaDePandora(numero){
-   if(numero % 2 == 0){
-    const binario = numero.toString(2);
-    return binario;
-   }else{
-    //numero hexadecimal
-    const hexadecimal = numero.toString(16);
-    return hexadecimal;
-   }
+// proximamente escribiremos codigo aqui
 }
-
-const esteban = ()=>{
-    return {
-        nombre: 'Esteban',
-        edad: 31,
-        nacionalidad: 'Uruguayo'
-    }
-}
-

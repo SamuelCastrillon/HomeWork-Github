6cbab0c502c154cab25cca2065fb1803b8873b54
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-08-19 19:41:33 -0300

    update-function-byrosario

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
+function laCajaDePandora(numero) {
+  if (typeof numero !== "number" || !Number.isInteger(numero)) {
+    console.log("No es un numero entero");
+  }
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
+}
+
+console.log(laCajaDePandora(8));
+console.log(laCajaDePandora(20));
+console.log(laCajaDePandora(12));
+console.log(laCajaDePandora(11));
